4c848eca5e3fb10347cac85b725e5140bcbd28dc
IcyBlue17 <96235160+IcyBlue17@users.noreply.github.com>
2022-07-08 11:27:48 +0800

    Add files via upload

---
+++ Inject.bat
@@ -0,0 +1,46 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDDKDLG7a
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWG+F5EdQ
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATEwkMgLXs=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRmy9VI1SA==
+::Zh4grVQjdCyDJGyX8VAjFDJXVQaOKW6GIrAP4/z0/9aurEcYV/ZxfZfeug==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+echo 正在注入SeewoAsssiant.exe
+echo ..........................
+choice /d y /t 1 >nul
+echo 25%已完成........
+choice /d y /t 3 >nul
+echo 50%已完成..........
+choice /d y /t 2 >nul
+echo 75%已完成...........
+choice /d y /t 3 >nul
+echo 注入成功！
+choice /d y /t 2 >nul
+pause
---
+++ Source.py
@@ -0,0 +1,58 @@
+from cgitb import text
+import os
+import tkinter as tk
+import time
+a=0
+#Commands
+def start():
+    #ml_s=cmd.get()
+    #ml=str(ml_s)
+    #os.system(ml)
+    os.system("start Koid.exe")
+def cmd():
+    win3=tk.Tk()
+    win3.title('信息中心电脑反控')
+    win3.geometry('400x200')
+    tk.Label(win3,text='成功连接到信息中心的计算机并使用MeteorPvP0x2019漏洞获取cmd！').pack()
+    tk.Label(win3,text='请输入您要在cmd内执行的内容').pack()
+    cmd=tk.Entry(win3).pack()
+    tk.Button(win3,text='执行',command=start).pack()
+    #os.system("start C:\Windows\System32\mstsc.exe")
+def kill():
+    os.system('taskkill /im winnt.exe /f')
+def delfile():
+    win4=tk.Tk()
+    win4.title("冰点还原一键关闭")
+    win4.geometry('400x200')
+    tk.Label(win4,text='冰点还原关闭程序',font=(None,15))
+
+    win4.mainloop()
+    #os.system('del D:/*')
+def get():
+    #os.system('fsutil file createnew /WindowsUpdate.vape 20000')
+    print('1')
+def abc():
+    os.system("start SeewoRunInject.exe")
+    time.sleep(13)
+    win2=tk.Tk()
+    win2.title("希沃管家注入控制台")
+    win2.geometry('400x200')
+    tk.Label(win2,text='希沃管家注入控制台',font=(None,15)).pack()
+    tk.Label(win2,text='注入成功！')
+    tk.Button(win2,text='杀死希沃管家所有进程',command=kill).pack()
+    tk.Button(win2,text='反控信息中心服务器',command=cmd).pack()
+    tk.Button(win2,text='强制关闭冰点还原',command=delfile).pack()
+    tk.Button(win2,text='管理员密码强行获取',command=get).pack()
+    tk.Label(win2,text='Ps：部分希沃会带有进程守护，强杀进程可能会导致系统崩溃')
+    win2.mainloop()
+#Command
+#r
+win=tk.Tk()
+win.title("希沃冰点还原破解工具 v2022.9")
+win.geometry("800x300")
+tk.Label(win,text='希沃冰点还原破解工具',font=("黑体",20)).pack()
+tk.Checkbutton(win,text='InjectHool反检测').pack()
+tk.Checkbutton(win,text='ZeloatModule+高级模式').pack()
+tk.Label(win,text='请输入您的学校代码,留空为自动识别:').pack(),tk.Entry(win,text='001035').pack()
+tk.Button(win,text='开始注入希沃管家进程：SeewoAsssiant.exe',command=abc).pack()
+win.mainloop()
